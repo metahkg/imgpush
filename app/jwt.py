5635e3b8ddf1a66eb9e420e523dc956b4391bc32
wcyat <wcyat@wcyat.me>
2023-02-15 22:52:01 +0800

    fix: fix and simplify jwt.py

--- app/jwt.py
+++ app/jwt.py
@@ -6,10 +6,14 @@ import settings
 def verify(token: str) -> dict | None:
     if settings.JWT_PUBLIC_KEY or settings.JWT_SECRET:
         try:
-            (_header, claims) = jwt.verify_jwt(token, pub_key=jwk.JWK.from_pem(open(settings.JWT_PUBLIC_KEY, "rb").read())
-                       if settings.JWT_PUBLIC_KEY else jwk.JWK.from_password(settings.JWT_SECRET),
-                       allowed_algs=[settings.JWT_ALGORITHM or
-                       ("RS256" if settings.JWT_PRIVATE_KEY else "HS256")], checks_optional=True)
+            claims = None
+            if settings.JWT_PUBLIC_KEY:
+                with open(settings.JWT_PUBLIC_KEY, "rb") as pub_key:
+                    (_header, claims) = jwt.verify_jwt(token, pub_key=jwk.JWK.from_pem(pub_key.read()),
+                       allowed_algs=[settings.JWT_ALGORITHM or "RS256"], checks_optional=True)
+            else:
+                (_header, claims) = jwt.verify_jwt(token, pub_key=jwk.JWK.from_password(settings.JWT_SECRET),
+                 allowed_algs=[settings.JWT_ALGORITHM or "HS256"], checks_optional=True)
             return claims
         except:
             return None
